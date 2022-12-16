6c59af0ed4672caff15fa41958f67e3f6af04d65
Ogunleye John Adegoke <hypescord@gmail.com>
2022-12-16 05:18:35 -0800

    added division

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
-oop = str[39:66]
-wpy = str[106:112] + str[0:6]
-print(oop + wpy)
+str = "Python is an interpreted, interactive, object-oriented program"
+str += "ming language that combines remarkable power with very clear syntax"
+oop = str[39:66] + str[106:112] + str[0:6]
+print(oop)
